6f9526b5c5869fd27ed6fc2155a8cc7ee595bb7b
ChristianYoha <christian.yoha@outlook.fr>
2021-10-29 17:49:10 +0200

    Configuration deploiement + Ajout modal contact

--- ecomm/components/Contact.tsx
+++ ecomm/components/Contact.tsx
@@ -1,12 +1,29 @@
 import styles from '../styles/Contact.module.scss'
+import { useState } from "react";
 
 export default function Contact() {
+
+    const [showMe, setShowMe] = useState(false);
+    function toggle(){
+        setShowMe(!showMe);
+    }
+
     return (
-        <div className={styles.contact}>
-            <h2 className={styles.contact__title}>Any questions ? <span>Contact us.</span></h2>
-            <a href="">
-                <button>Contact Us</button>
-            </a>
-        </div>
+        <section>
+            <div className={styles.contact}> 
+                <h2 className ={styles.contact__title}>Any questions ? Contact Us.</h2>
+                <button onClick={toggle}>Contact Us</button>
+            </div>
+            <div className={ showMe ? styles.modal__active : styles.modal}>
+                <h2 className={styles.modal__title}>Contact Us</h2>
+                <form action="#" className={ styles.modal__form}>
+                    <label htmlFor="email">Email :</label>
+                    <input type="email" name="email" id="email" />  
+                    <label htmlFor="message">Message :</label>
+                    <textarea name="message" id="message"></textarea>   
+                    <input type="submit" value="Envoyer" onClick={toggle}/>
+                </form>
+            </div>
+        </section>
     )
 }
